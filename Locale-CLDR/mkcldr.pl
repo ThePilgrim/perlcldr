1b8121a9b742962452327fb067e9c8fb3ac783fd
John Imrie <john.imrie1@gmail.com>
2018-08-05 08:01:18 +0100

    Aded test for transliteration using another transliteration

--- Locale-CLDR/mkcldr.pl
+++ Locale-CLDR/mkcldr.pl
@@ -36,7 +36,7 @@ use version;
 my $API_VERSION = 0; # This will get bumped if a release is not backwards compatible with the previous release
 my $CLDR_VERSION = '33'; # This needs to match the revision number of the CLDR revision being generated against
 my $REVISION = 0; # This is the build number against the CLDR revision
-my $TRIAL_REVISION = ''; # This is the trial revision for unstable releases. Set to '' for the first trial release after that start counting from 1
+my $TRIAL_REVISION = '1'; # This is the trial revision for unstable releases. Set to '' for the first trial release after that start counting from 1
 our $VERSION = version->parse(join '.', $API_VERSION, ($CLDR_VERSION=~s/^([^.]+).*/$1/r), $REVISION);
 my $CLDR_PATH = $CLDR_VERSION;
 
